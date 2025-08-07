3db670d44828ac144bea1a0907fd416a951e8c94
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-07 17:43:13 +0000

    Users#show mostly finished; need to add edit form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index"})
+  # Users
   get("/users", { :controller => "users", :action => "index"})
+  get("/users/:path_username", { :controller => "users", :action => "show"})
+
 end
