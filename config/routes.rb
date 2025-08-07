8e193715769455ed4df40728d3c6fe22f1030726
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-07 18:12:00 +0000

    finished photos#index and #show; need to add edit/delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "users", :action => "index"})
+
   # Users
   get("/users", { :controller => "users", :action => "index"})
   get("/users/:path_username", { :controller => "users", :action => "show"})
+
+  #Photos
+  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
 
 end
