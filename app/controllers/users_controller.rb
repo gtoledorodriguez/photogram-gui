11cd70888380b300790a999f31871d9b326eb3e9
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-09 08:04:32 +0000

    finished users#create and users#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,30 @@ class UsersController < ApplicationController
       render({ :template => "user_templates/show" })
     end
   end
+
+  def create
+    input_username = params.fetch("input_username")
+    @the_user = User.new
+    @the_user.username = input_username
+    if @the_user.valid?
+      @the_user.save
+      redirect_to("/users/#{@the_user.username}", { :notice => "User created successfully."})
+    else
+      redirect_to("/users", { :notice => "User failed to created successfully."})
+    end
+  end
+
+  def update
+    input_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => input_username })
+    @the_user = matching_usernames.at(0)
+    @the_user.username = params.fetch("input_username")
+    if @the_user.valid?
+      @the_user.save
+      redirect_to("/users/#{@the_user.username}", { :notice => "User created successfully."})
+    else
+      redirect_to("/users/#{@the_user.username}", { :notice => "User failed to created successfully."})
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "users", :action => "index"})
+  get("/", { :controller => "users", :action => "index" })
 
   # Users
-  get("/users", { :controller => "users", :action => "index"})
-  get("/users/:path_username", { :controller => "users", :action => "show"})
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:path_username", { :controller => "users", :action => "show" })
+  post("/add_user", { :controller => "users", :action => "create" })
+  post("/update_user/:path_username", { :controller => "users", :action => "update" })
 
   #Photos
-  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos", { :controller => "photos", :action => "index" })
   post("/insert_photo", { :controller => "photos", :action => "create" })
-  get("/photos/:path_id", { :controller => "photos", :action => "show"})
-  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
   post("/update_photo/:path_id", { :controller => "photos", :action => "update" })
   post("/add_comment/", { :controller => "photos", :action => "add_comment" })
 end
